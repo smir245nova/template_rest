280a2589458fe019cd8fea36565cc959b8fe12e0
smir245nova <81694540+smir245nova@users.noreply.github.com>
2022-11-26 00:33:27 +0300

    Initial commit

---
+++ src/main/java/ru/my/Application.java
@@ -0,0 +1,18 @@
+package ru.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        Communication communication = context.getBean("communication", Communication.class);
+        // System.out.println(communication.getAllUsers());
+        System.out.println("Answer: " + communication.getAnswer());
+    }
+}
+
---
+++ src/main/java/ru/my/config/JmConfig.java
@@ -0,0 +1,29 @@
+package ru.my.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+@Configuration
+@ComponentScan("ru.its.resttemplate")
+public class JmConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
+
